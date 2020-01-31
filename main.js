d5a5253e6ded8817ee09a4964e02215678314bec
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2020-01-31 16:42:10 -0600

    Display winner message and finish game resetting

--- main.js
+++ main.js
@@ -25,27 +25,47 @@ let Board = (function(){
             square.textContent = '';
         });
         Game.gameOver = false;
+        Game.end();
         currentTurn = 'x';
     }
     return{element, squares, place, clear, addSquareListeners};
 })();
-let User = (function(){
-    let name = '';
-    return {};
-})();
+
 let Game = (function(){
+    let player1Name = 'Player 1';
+    let player2Name = 'Player 2';
+    let winner = '';
     let gameOver = false;
+    let run = function(){
+        Board.addSquareListeners();
+    }
+    let start = function(){
+        let player1Input =document.querySelector('#player1-name').value;
+        let player2Input =document.querySelector('#player2-name').value;
+        if(player1Input != ''){
+            player1Name = player1Input;
+        }
+        if(player2Input != ''){
+            player2Name = player2Input;
+        }
+    }
+    let end = function(){
+        winnerHeader.textContent = '';
+    }
     let checkForWinner = function(){
         if(isInLine(0,1,2,'X')|| isInLine(3,4,5,'X')||isInLine(6,7,8,'X')||
         isInLine(0,3,6,'X')||isInLine(1,4,7,'X')||isInLine(2,5,8,'X')||
         isInLine(0,4,8,'X')||isInLine(2,4,6,'X')){
-            console.log('x wins');
+            winner = player1Name;
+            winnerHeader.textContent = `${winner} wins!`;
             Game.gameOver = true;
         }
         if(isInLine(0,1,2,'O')|| isInLine(3,4,5,'O')||isInLine(6,7,8,'O')||
         isInLine(0,3,6,'O')||isInLine(1,4,7,'O')||isInLine(2,5,8,'O')||
         isInLine(0,4,8,'O')||isInLine(2,4,6,'O')){
             console.log('o wins');
+            winner = player2Name;
+            winnerHeader.textContent = `${winner} wins!`;
             Game.gameOver = true;
         }
     }
@@ -54,23 +74,27 @@ let Game = (function(){
                 Board.squares[b].textContent == string && 
                 Board.squares[c].textContent == string);
     }
-    return {checkForWinner, gameOver};
+    return {run, start, end, checkForWinner, gameOver};
 })();
 
 let startForm = document.querySelector('#start-game-form');
 let startButton = document.querySelector('#start-game');
 let exitButton = document.querySelector('#exit-game');
 let newGameButton = document.querySelector('#new-game');
+let winnerHeader = document.querySelector('#winner');
 
 startButton.addEventListener('click',e=>{
     Board.element.classList.remove('invisible');
     startForm.classList.add('invisible');
+    Game.start();
 });
 exitButton.addEventListener('click', e=>{
     Board.element.classList.add('invisible');
     startForm.classList.remove('invisible');
+    Board.clear();
+    Game.end();
 });
 newGameButton.addEventListener('click',e=>{
     Board.clear();
 });
-Board.addSquareListeners();
+Game.run();
